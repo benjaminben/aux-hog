e21ce6364549ffdc719b9e4d8b2263f471d26350
benBen <benmbenjamin@gmail.com>
2017-03-11 18:20:16 -0800

    okay its working......

--- controllers/welcome.js
+++ controllers/welcome.js
@@ -4,28 +4,32 @@ var Circle  = require('../models/circle');
 var index = function(req, res, next) {
 
   if (req.user) {
-    console.log(req.user.circles.length);
     var allUserCircles = [];
 
     if (req.user.circles.length > 0) {
+      var itemsProcessed = 0;
+      req.user.circles.forEach(function(circle) {
+        Circle.findById(circle._id).populate('users').exec(function(err, circle) {
+            if (err) {
+              console.log(err)
+            } else {
+              allUserCircles.push(circle);
+              itemsProcessed++;
 
-    var itemsProcessed = 0;
-    req.user.circles.forEach(function(circle) {
-      Circle.findById(circle._id).populate('users').exec(function(err, circle) {
-          if (err) {
-            console.log(err)
-          } else {
-            allUserCircles.push(circle);
-            itemsProcessed++;
-
-            if (itemsProcessed === req.user.circles.length) {
-              console.log(allUserCircles);
-              res.render('./index', { user: req.user, userCircles: allUserCircles });
-              // res.render('./index', { user: req.user, userCircles: allUserCircles });
+              if (itemsProcessed === req.user.circles.length) {
+                console.log("yup yup");
+                res.render('./index',
+                  {
+                    user: req.user,
+                    bloobs: 'bloobs',
+                    userCircles: allUserCircles
+                  }
+                );
+                // res.render('./index', { user: req.user, userCircles: allUserCircles });
+              }
             }
-          }
+        })
       })
-    })
     }
     else {
       console.log("squuuuaa");
